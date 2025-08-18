495ff3d6a251afff986a81fcc1a94c2968fc44d9
Kathleen Santos <kathleen.santos@csacademy.com.br>
2025-08-18 17:26:15 -0300

    remove files retroactively

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import 'express-async-errors'
+
 import { router } from "./routes";
 
 const app = express();
 app.use(express.json())
 
 app.use(router);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof Error) {
+        return res.status(400).json({
+            error: error.message
+        })
+    }
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'internal server error!!!'
+    })
+})
 
 app.listen(3333, () => console.log("Servidor online!"))
